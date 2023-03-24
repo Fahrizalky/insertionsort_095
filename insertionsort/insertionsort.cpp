735904b986461f12e5362527d86de57b54838df7
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-03-24 14:53:37 +0700

    masukkan element array

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -15,6 +15,16 @@ void input() {
 		else
 		{
 			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << " Masukan Element Array" << endl;
+	cout << "======================" << endl;
 
-	
- }
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-n" << (i + 1) << ":";
+		cin >> arr[i];
+	}
+}
